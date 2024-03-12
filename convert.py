48469021bdeba765d1b8d726b0d97533c32f71ba
micwonder <130264564+micwonder@users.noreply.github.com>
2024-03-12 14:52:30 -0500

    Optimized the codebase

--- convert.py
+++ convert.py
@@ -9,8 +9,8 @@ from tqdm import tqdm
 
 def convert_files(
     src_dir: str,
-    dst_dir: str = "",
-    image_format: str = "",
+    dst_dir: str = None,
+    image_format: str = None,
     out_fname_format: str = "%d_%u",
     delete_source: bool = False,
     logger: Logger = Logger(),
@@ -18,8 +18,8 @@ def convert_files(
     """Convert filenames of every images to uuid format.
 
     ### Function Arguments:
-     - root_dir: absolute path of source directory
-     - dst_dir `optional`: absolute path of destination directory; default = `root_dir`
+     - src_dir: absolute path of source directory
+     - dst_dir `optional`: absolute path of destination directory; default = `src_dir`
      - image_format `optional`: format of output image; defaults to save original format
      - out_fname_format `optional`: template string for output image filename; defaults to `%d_%u`; `%u`: `uuid_v4`; `%d`: `timestamp`
      - delete_source `optional`: delete source images; defaults to `False`
@@ -29,7 +29,7 @@ def convert_files(
 
     ```python
     convert_files(
-      root_dir="C:/Users/Admin/Pictures/sample",
+      src_dir="C:/Users/Admin/Pictures/sample",
       dst_dir="c:/users/admin/pictures/normalized",
       image_format="png",
       out_fname_format="thermal_%d_%u",
@@ -37,81 +37,63 @@ def convert_files(
     )
     ```
     """
-    root_dir = src_dir
+    dst_dir = dst_dir or src_dir
+    os.makedirs(dst_dir, exist_ok=True)
+    src_dir = src_dir
 
-    if not root_dir or not os.path.exists(root_dir) or not os.path.isdir(root_dir):
-        return logger.error(f"Cannot read {root_dir}")
+    total_file_count, success_count, skipped_count = 0, 0, 0
+    file_count = sum(len(files) for _, _, files in os.walk(src_dir))
 
-    if not dst_dir:
-        dst_dir = root_dir
-
-    if not os.path.exists(dst_dir):
-        logger.info(f"Directory {dst_dir} created.")
-        os.mkdir(dst_dir)
-
-    tot_filecnt, success_cnt, skipped_cnt = 0, 0, 0
-
-    cpt = sum([len(files) for _, _, files in os.walk(root_dir)])
-
-    # for filename in os.listdir(root_dir):
     with tqdm(
-        total=cpt, desc="Copying files" if not image_format else "Imwriting files"
+        total=file_count,
+        desc="Copying files" if not image_format else "Imwriting files",
     ) as pbar:
-        for subdir, dirs, files in os.walk(root_dir):
+        for subdir, _, files in os.walk(src_dir):
             for filename in files:
-                _, src_ext = os.path.splitext(filename)
-                dst_ext = "." + image_format if image_format else src_ext
+                src_path = os.path.join(subdir, filename)
+                if not os.path.isfile(src_path):
+                    continue
 
-                # src = root_dir + "\\" + filename
-                src = os.path.join(subdir, filename)
+                _, src_ext = os.path.splitext(filename)
+                dst_ext = f".{image_format}" if image_format else src_ext
                 dst_filename = out_fname_format.replace(
                     "%u", str(uuid.uuid4())
                 ).replace("%d", str(datetime.datetime.now().timestamp()))
                 dst_filename += dst_ext
+
                 updated_src = os.path.join(subdir, dst_filename)
-                relative_path = os.path.relpath(updated_src, root_dir)
-                dst = os.path.join(dst_dir, relative_path)
+                relative_path = os.path.relpath(updated_src, src_dir)
+                dst_path = os.path.join(dst_dir, relative_path)
 
-                if not os.path.isfile(src):
-                    continue
-
-                # Ensure the destination directory exists
-                os.makedirs(os.path.dirname(dst), exist_ok=True)
-
-                tot_filecnt += 1
+                os.makedirs(os.path.dirname(dst_path), exist_ok=True)
+                total_file_count += 1
                 pbar.update(1)
 
                 if image_format:
                     try:
-                        img = iio.imread(src)
-                    except:
-                        logger.error(f"{src} is not an image. skipped")
-                        skipped_cnt += 1
+                        img = iio.imread(src_path)
+                        iio.imwrite(dst_path, img)
+                        if delete_source:
+                            os.remove(src_path)
+                    except Exception as e:
+                        logger.error(f"Error processing image {src_path}: {e}")
+                        skipped_count += 1
                         continue
-
-                    try:
-                        iio.imwrite(dst, img)
-                    except:
-                        logger.error(f"Error saving image to {dst}.")
-                        continue
-
-                    if delete_source:
-                        try:
-                            os.remove(src)
-                        except:
-                            logger.error(f"Error removing {src}")
 
                 else:
                     try:
                         if delete_source:
-                            os.rename(src, dst)
+                            os.rename(src_path, dst_path)
                         else:
-                            copyfile(src, dst)
+                            copyfile(src_path, dst_path)
                     except:
-                        logger.error(f"Error handling file {src} to {dst}")
+                        logger.error(
+                            f"Error copying file from {src_path} to {dst_path}: {e}"
+                        )
+                        continue
 
-                success_cnt += 1
+                success_count += 1
 
-    message = f"Converted {success_cnt} images among {tot_filecnt} files. {skipped_cnt} files skipped."
+    message = f"Converted {success_count} images among {total_file_count} files. {skipped_count} files skipped."
     logger.info(message)
     print(message)
